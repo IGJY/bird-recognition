5cbd6a49605956066b2bd2bdf308d36a7255a430
IGJY <10619977+igjy@user.noreply.gitee.com>
2024-05-20 16:37:35 +0800

    增加从文件中提取MFCC特征并且保存为npy文件功能

---
+++ code/MFCC_S.py
@@ -0,0 +1,32 @@
+import ntpath
+import os
+import sys
+import time
+
+import librosa
+from librosa import feature
+import numpy as np
+
+import torch
+from tqdm import tqdm
+
+
+# 153516 138361 6904 8251
+
+def get_MFCC(route):
+
+    wav_file = route
+    wav, sr = librosa.load(wav_file, sr=16000, duration=2.0)  # 具体的数据设置不需要修改，duration代表的就是提取的音频文件为2s
+
+    # 计算音频信号的MFCC
+    label = float(ntpath.basename(os.path.dirname(route)))  # 数据的标签
+
+    mfcc_delta0 = librosa.feature.mfcc(y=wav, sr=sr, n_mfcc=60)  # 提取MFCC特征，维度为60维
+    mfcc_delta1 = librosa.feature.delta(mfcc_delta0)  # 一阶差分变换
+    mfcc_delta2 = librosa.feature.delta(mfcc_delta1, order=2)  # 二阶差分变换，作用是为了增加动态信息，也是比较传统的做法
+
+    mfcc = np.vstack([mfcc_delta0, mfcc_delta1, mfcc_delta2])  # 将MFCC和经过差分变换后的进行拼接,此时的shape为[-1,180,60,63]
+    mfcc = mfcc.reshape([3, 60, 63])  # 修改shape 为[60，63]
+
+    return mfcc, label
+    # return mfcc
---
+++ code/Save_MFCC.py
@@ -0,0 +1,39 @@
+# 保存GFCC到本地
+import os
+import sys
+import librosa
+import numpy as np
+from tqdm import tqdm
+import torch
+from MFCC_S import get_MFCC
+
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
+
+
+files = '..\\data\\BirdsSong-10spec-graduation'  # 音频文件的地址，为各个分类的上一级
+all_mfcc = []
+all_label = []
+# jishu = 0
+for file in os.listdir(files):
+    wav_files = files + '\\' + file
+    print(wav_files)
+    for wav_file in tqdm(os.listdir(wav_files)):
+        file_1 = wav_files + '\\' + wav_file
+        # print(ntpath.basename(os.path.dirname(file_1)))
+        mfcc, label = get_MFCC(route=file_1)
+        # print(gfcc)
+        all_mfcc.append(mfcc)
+        all_label.append(label)
+        # print(all_label)
+        # jishu +=1
+        # print(jishu)
+mfcc_train = np.array(all_mfcc)
+label_train = np.array(all_label)
+mfcc_train = torch.tensor(mfcc_train)
+label_train = torch.tensor(label_train)
+label_train = torch.squeeze(label_train)
+
+np.save('../data/MFCC_train/MFCC_train_sample.npy', mfcc_train)
+np.save("../data/MFCC_train/MFCC_train_label.npy", label_train)
+
+print("保存完毕")
